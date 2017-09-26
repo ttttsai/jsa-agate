b6298b2edea865079f2f351c264f83e7fae03174
Jessie Cai <tsai19930224@gmail.com>
2017-09-26 16:34:16 +0800

    webconfiguration again and again and again

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src') + '/app/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),// + '/app',
+    path: path.resolve(__dirname, 'dist') + '/app',
     filename: 'bundle.js',
     publicPath: '/app/'
   },

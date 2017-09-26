c563823babe312b78612eabb03f62a3f08372b0d
linjialiang1234 <linjialiang86@163.com>
2017-09-26 14:33:06 +0800

    AGTA-1 add React Heading

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 require('./scss/styles.scss');
-console.log("askdjahkjd");
+
+class ReactHeader extends React.Component{
+    render(){
+        return (
+        <div>
+          <h1>Hello World!</h1>
+        </div>);
+    }
+}
+ReactDOM.render(<ReactHeader />, document.querySelector(".center"));

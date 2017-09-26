f681d1873544337aeaa6f7efff6e29f247794141
linjialiang1234 <31198266+linjialiang1234@users.noreply.github.com>
2017-09-26 18:00:28 +0800

    AGT-4 add React Header (#5)
    * AGT-4 add React Header
    
    * AGT-4 add change

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,14 @@
-require('./scss/styles.scss');
-console.log("Meaningful Message!!!!"); 
+const React = require('react');
+const ReactDOM = require('react-dom');
+const HeaderStyle = require('./scss/styles.scss');
+
+class ReactHeader extends React.Component{
+  render() {
+    return (
+      <div>
+        <header>Hello World!</header>
+      </div>);
+  }
+}
+
+ReactDOM.render(<ReactHeader />, document.getElementById('root'));

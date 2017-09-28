e551f9a9338db46ea902b54877f6c9e83228172e
linjialiang1234 <31198266+linjialiang1234@users.noreply.github.com>
2017-09-28 11:01:11 +0800

    AGT-5 add frontend test (#10)
    * AGT-5 add frontend test
    
    * AGT-5 Add frontend test

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const ReactDOM = require('react-dom');
+const ReactHeader = require('./app/component/ReactHeader');
+
+ReactDOM.render(<ReactHeader />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src') + '/app/index.js',
+  entry: path.resolve(__dirname, 'src') + '/client/index.js',
   output: {
     path: path.resolve(__dirname, 'dist') + '/app',
     filename: 'bundle.js',

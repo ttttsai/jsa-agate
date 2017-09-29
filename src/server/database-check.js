a17f983cf9862b6466939a594856554de8506888
Zhuowei Liu <yenmoon@icloud.com>
2017-09-29 03:10:23 -0500

    Env variable heroku (#19)
    * AGT-8
    
    * AGT-8 fix eslint require-jsdoc error in inde.js
    
    * refactor AGT-8
    
    * make it compatible in heroku by using envrionment variables
    
    * change config to function

--- src/server/database-check.js
+++ src/server/database-check.js
@@ -1,12 +1,19 @@
 'use strict';
 
-const config = require('../../.application-config.json');
 const MongoClient = require('mongodb').MongoClient;
 
+const config = function() {
+  if (process.env.NODE_ENV !== 'production') {
+    return require('../../.application-config.json');
+  } else {
+    return {};
+  }
+}();
+
 const createDatabaseUrl = function() {
-  const address = config.database.url;
-  const port = config.database.port;
-  const databaseName = config.database.databaseName;
+  const address = config.database.url || process.env.MONGO_ADDRESS;
+  const port = config.database.port || process.env.MONGO_PORT;
+  const databaseName = config.database.databaseName || process.env.MONGO_DBNAME;
   return `${address}:${port}/${databaseName}`;
 };
 

af12a8a2b66a545cbe9917871389684aef654f70
ttttsai <tsai19930224@gmail.com>
2017-09-29 10:50:39 +0800

    Improve webpack file and eslint related (#12)
    * Improve webpack file and eslint related
    
    * update eslint file
    
    * set environment value for browser and node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,41 @@
 module.exports = {
-  "extends": "google"
+  'extends': 'google',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+  },
+  'env': {
+    'es6': true,
+  },
+  'rules': {
+    'require-jsdoc': 0,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-uses-react': 2,
+  },
+  'plugins': [
+    'react',
+  ],
+  'overrides': [
+    {
+      'files': ['src/client/**/*.js'],
+      'parserOptions': {
+        'sourceType': 'module',
+      },
+      'env': {
+        'browser': true,
+      },
+    },
+    {
+      'files': [
+        'webpack.*.js',
+        '.eslintrc.js',
+        'test/**/*.test.js',
+        'src/server/**/*.js',
+      ],
+      'env': {
+        'node': true,
+      },
+    },
+  ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-var path = require('path');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src') + '/client/index.js',
+  entry: './src/client/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist') + '/app',
+    path: path.resolve(__dirname, 'dist/app'),
     filename: 'bundle.js',
     publicPath: '/app/'
   },
@@ -13,16 +15,29 @@ module.exports = {
       include: path.resolve(__dirname, 'src'),
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015']
+        presets: ['react', 'env']
       }
     },
     {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    },
-    {
       test: /\.scss$/,
-      loader: 'style-loader!css-loader!sass-loader'
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'expanded',
+            },
+          },
+        ],
+      }),
     }]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin([path.resolve(__dirname, './dist/app')]),
+    new ExtractTextPlugin('style.css'),
+  ],
 };

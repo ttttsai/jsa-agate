a80c7263ddfd6ef50769e559a392a7bac94dc152
ttttsai <tsai19930224@gmail.com>
2017-10-13 11:19:42 +0800

    fix map on heroku (#38)
    * delete async of heroku
    
    * map bug fix
    
    * use const instead of let

--- src/client/app/component/HomePageMap/index.js
+++ src/client/app/component/HomePageMap/index.js
@@ -5,30 +5,38 @@ import './style.scss';
 class HomePageMap extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
-  componentDidMount() {
+  componentWillMount() {
     window.initMap = this.initMap.bind(this);
     loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyAHP4cn0A4W4VIudAlmHmpAakBvbmcR5fY&callback=initMap');
   }
   initMap() {
-    let center = {lat: 22.2222, lng: 114};
-    let mapProp = {
+    const center = {lat: 22.2222, lng: 114};
+    const mapProp = {
       center: center,
       zoom: 9,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
-    let map = new google.maps.Map(
+    const map = new google.maps.Map(
       document.getElementsByClassName('home-page-map')[0], mapProp);
-    if (this.props.businesses) {
+    this.setState({map: map});
+    this.makeMarkers();
+  }
+  makeMarkers() {
+    const that = this;
+    if (this.props.businesses && this.state.map) {
       this.props.businesses.forEach(function(value) {
-        let marker = new google.maps.Marker({
+        const marker = new google.maps.Marker({
           position: {lat: value.latitude, lng: value.longitude},
-          map: map,
+          map: that.state.map,
         });
       });
     }
   }
   render() {
+    this.makeMarkers();
     return <div className="home-page-map" ></div>;
   }
 }

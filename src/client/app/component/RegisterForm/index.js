9edd2464f19d75f620c238eda8c8fd811ce675d8
linjialiang1234 <31198266+linjialiang1234@users.noreply.github.com>
2017-10-17 11:05:17 +0800

    feat(agt-22): register functionality (#46)
    * copy registerForm
    
    * fix css
    
    * fix register form
    
    * Function Register
    
    * fix elsint
    
    * AGT-22-Register
    
    * AGT-22
    
    * change Register
    
    * change password format
    
    * fix password format

--- src/client/app/component/RegisterForm/index.js
+++ src/client/app/component/RegisterForm/index.js
@@ -1,26 +1,50 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {Spin} from 'antd';
+import 'antd/lib/spin/style/index.css';
 import './style.scss';
 
 class RegisterForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {loading: false};
+  }
+  goToLoginPage() {
+    this.props.history.push('/login');
   }
   render() {
+    const formClassNames = this.props.formHasError ?
+      'register-box error-form-box' : 'login-box';
+    const {loading, onSubmit, errMsg} = this.props;
+    const passwordFormat = '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])' +
+    '[A-Za-z\\d$@$!%*#?&]{6,50}$';
+
     return (
-      <div className="register-box">
-        <h1>Sign up</h1>
-        <form>
-          <input type="email" name="email" required placeholder="E-mail" />
-          <input type="password" name="password" required
-            placeholder="Password"/>
-          <input type="password" name="retype-password" required
-            placeholder="Re-type Password"/>
-          <input type="submit" value="Register me!"/>
-        </form>
-        <button>Log in</button>
+      <div className={formClassNames}>
+        <Spin spinning={loading}>
+          <h1>Sign up</h1>
+          <form method="post" onSubmit={onSubmit}>
+            <p className="form-error-message">{errMsg}</p>
+            <input title="Username must be 3 to 15 letters" type="text"
+              name="username" placeholder="Username" pattern="[A-Za-z]{3,15}"
+              required />
+            <input title="Minimum six characters, at least one letter,
+            one number and one special character"
+              type="password" name="password" placeholder="Password"
+              pattern={passwordFormat}
+              required/>
+            <input title="Minimum six characters, at least one letter,
+            one number and one special character"
+              type="password" name="retype-password"
+              placeholder="Re-type Password" pattern={passwordFormat}
+              required/>
+            <input type="submit" value="Register me!"/>
+          </form>
+          <button onClick={this.goToLoginPage.bind(this)}>Log in</button>
+        </Spin>
       </div>
     );
   }
 }
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
--- src/client/app/component/RegisterPage/index.js
+++ src/client/app/component/RegisterPage/index.js
@@ -1,15 +1,87 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import RegisterForm from '../RegisterForm';
 import Header from '../CommonHeader';
 import './style.scss';
 
 class RegisterPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'loading': false,
+      'successRegister': false,
+      'formHasError': false,
+    };
+  }
+  submitHandler(event) {
+    this.setState({'loading': true});
+    event.preventDefault();
+    let password = event.target.elements[1].value;
+    let retypePassword = event.target.elements[2].value;
+
+    this.validatePasswords(password, retypePassword);
+  }
+
+  validatePasswords(password, retypePassword) {
+    if (password != retypePassword) {
+      this.setState({'loading': false});
+      this.setState({
+        'errMsg': 'Passwords are not the same.',
+        'formHasError': true,
+      });
+    } else {
+      this.submitData({
+        username: event.target.elements[0].value,
+        password: password,
+      });
+    }
+  }
+  submitData(data) {
+    let that = this;
+    let myHeaders = new Headers();
+    let myInit = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: myHeaders,
+    };
+
+    myHeaders.append('content-type', 'application/json');
+
+    fetch('/api/register', myInit).then(function(response) {
+      return response.json();
+    }).then(function(value) {
+      that.setState({'loading': false});
+      if (value.status === '409') {
+        throw new Error(value.status);
+      } else {
+        that.successHandler();
+      }
+    }).catch(function(err) {
+      that.errorHandler(err);
+    });
+  }
+  successHandler() {
+    this.setState({'errMsg': '', 'formHasError': false});
+    this.setState({'successRegister': true});
+  }
+  errorHandler(err) {
+    if (err.message == '409') {
+      this.setState({
+        'errMsg': 'User name has been used.',
+        'formHasError': true,
+      });
+    }
+  }
   render() {
-    return (
+    const {successRegister, loading, errMsg, formHasError} = this.state;
+
+    return successRegister ? (<Redirect to="/login" />) : (
       <div className="register-page">
         <Header />
         <div className="register-page-content">
-          <RegisterForm />
+          <RegisterForm onSubmit={this.submitHandler.bind(this)}
+            loading={loading} errMsg={errMsg}
+            formHasError={formHasError} />
         </div>
       </div>
     );

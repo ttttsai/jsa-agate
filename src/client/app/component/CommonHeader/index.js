6b8896c81008f4c413530275c0abb621c467a594
linjialiang1234 <31198266+linjialiang1234@users.noreply.github.com>
2017-10-23 11:50:25 +0800

    fix(agt-49): link to home page on single business view (#65)

--- src/client/app/component/CommonHeader/index.js
+++ src/client/app/component/CommonHeader/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './style.scss';
 
 class CommonHeader extends React.Component {
+  componentDidMount() {
+    const that = this;
+
+    this.refs.menuButton.addEventListener('click', function(event) {
+      that.props.history.push('/');
+    });
+  }
+
   render() {
     return (
       <div className="common-header">
-        <div className="menu"></div>
+        <div className="menu" ref="menuButton"></div>
         <div className="avatar"></div>
       </div>
     );
   }
 }
 
-export default CommonHeader;
+export default withRouter(CommonHeader);
